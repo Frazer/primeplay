c713b93457572a7dafbb589e2ee3d87e593268e9
Frazer Kirkman <frazer@unitedvisions.org>
2014-08-22 13:57:02 +0930

    had prime2 knock out all even numbers

--- primeplay.c
+++ primeplay.c
@@ -2,11 +2,12 @@
 #define CHECKUPTO 10000
 
 
-struct primes
+typedef struct PRIMEPLAYITERATOR
 {
 	int prime;
-	int iteratedUpTo;
-};
+	int currentIteratedNumber ;
+	int previousIteratedNumber ;
+} PRIMEPLAYITERATOR;
 
 int main(){
 /*
@@ -18,11 +19,45 @@ int main(){
 	for(int i=0;i<CHECKUPTO;++i) {
 		primePlay[i] = 1+i;
 	}
-	int currentIteratedNumber;
-	int previousIteratedNumber;
+/*
+ *
+ * at this point each position in the array points to the next position
+ * by the algorithms process, this is equivalent to running through with 
+ * a 'prime' of 1
+ *
+ */
+	int currentPrime = 2;
+	PRIMEPLAYITERATOR cp = {currentPrime,currentPrime+1,currentPrime};	
+	
+	for(; cp.currentIteratedNumber < CHECKUPTO; ) {     /*   will check up to previous iterator */
+		if (cp.currentIteratedNumber%cp.prime){
+		/*
+		 * not a multiple, so can skip and move on
+		 *
+		 */
+			cp.previousIteratedNumber = cp.currentIteratedNumber;
+			cp.currentIteratedNumber = primePlay[cp.currentIteratedNumber];
 
-	for(int j = 0; j < CHECKUPTO; j++) {
-	     printf("%d ", primePlay[j]);
+		}else{
+		/*
+		 * is a multiple, so should be knocked out
+		 * so previous number needs to point to where this is pointing
+		 * and set this to 0.
+		 */
+			primePlay[cp.previousIteratedNumber] = primePlay[cp.currentIteratedNumber];
+			primePlay[cp.currentIteratedNumber]=0;
+			cp.currentIteratedNumber = primePlay[cp.previousIteratedNumber]; 
+		}
+
+	}
+
+	
+	
+	
+	for(int j = 1; j < CHECKUPTO; j++) {
+	  if (primePlay[j]){
+	     printf("%d ",j);
+	  }
 	}
 	printf("\n");
 /*
