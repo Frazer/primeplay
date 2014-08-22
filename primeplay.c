a29bc50d5da8de4bde71e33843c9b3d662a537d2
Frazer Kirkman <frazer@unitedvisions.org>
2014-08-22 15:36:19 +0930

    made it find all primes and output the steps for each knockout

--- primeplay.c
+++ primeplay.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#define CHECKUPTO 10000
+#include <math.h>
+#define CHECKUPTO 1000000
 
 
 typedef struct PRIMEPLAYITERATOR
@@ -26,38 +27,76 @@ int main(){
  * a 'prime' of 1
  *
  */
-	int currentPrime = 2;
-	PRIMEPLAYITERATOR cp = {currentPrime,currentPrime+1,currentPrime};	
-	
-	for(; cp.currentIteratedNumber < CHECKUPTO; ) {     /*   will check up to previous iterator */
-		if (cp.currentIteratedNumber%cp.prime){
+
+	int currentPrime = 1;
+	int maxPrimeIteror = sqrt(CHECKUPTO)-6;
+	while(currentPrime< maxPrimeIteror){
+		currentPrime = primePlay[currentPrime];
+		int startingPrimeSquared = currentPrime*currentPrime;
+
 		/*
-		 * not a multiple, so can skip and move on
+		 * jumps to prime^2
+		 * and needs to step back to find which place points to it
 		 *
 		 */
-			cp.previousIteratedNumber = cp.currentIteratedNumber;
-			cp.currentIteratedNumber = primePlay[cp.currentIteratedNumber];
-
-		}else{
-		/*
-		 * is a multiple, so should be knocked out
-		 * so previous number needs to point to where this is pointing
-		 * and set this to 0.
-		 */
-			primePlay[cp.previousIteratedNumber] = primePlay[cp.currentIteratedNumber];
-			primePlay[cp.currentIteratedNumber]=0;
-			cp.currentIteratedNumber = primePlay[cp.previousIteratedNumber]; 
+		int placePointingAtPrimeSquared = 1; 
+		for(int j = startingPrimeSquared-1; j >1 ; j--) {
+		  if (primePlay[j]){
+		     placePointingAtPrimeSquared = j;
+		     break;
+		  }
 		}
 
-	}
+		PRIMEPLAYITERATOR cp = {currentPrime,startingPrimeSquared,placePointingAtPrimeSquared};
+		
+		printf("prime:%d :",cp.prime);
+		int  counter = 0;
+		int  numberOfCountersPrinted = 0;
+		for(; cp.currentIteratedNumber < CHECKUPTO; ) {     /*   will check up to previous iterator */
+			if (cp.currentIteratedNumber%cp.prime){
+			/*
+			 * not a multiple, so can skip and move on
+			 *
+			 */
+				cp.previousIteratedNumber = cp.currentIteratedNumber;
+				cp.currentIteratedNumber = primePlay[cp.currentIteratedNumber];
+				counter++;
 
-	
-	
-	
-	for(int j = 1; j < CHECKUPTO; j++) {
-	  if (primePlay[j]){
-	     printf("%d ",j);
-	  }
+			}else{
+			/*
+			 * is a multiple, so should be knocked out
+			 * so previous number needs to point to where this is pointing
+			 * and set this to 0.
+			 */
+				
+				if (numberOfCountersPrinted<19){
+					printf("%d, ",counter+1); 
+			/*		printf("%d[%d], ",counter+1,cp.currentIteratedNumber);  */
+					counter=0;
+					numberOfCountersPrinted++;
+				}
+				primePlay[cp.previousIteratedNumber] = primePlay[cp.currentIteratedNumber];
+				primePlay[cp.currentIteratedNumber]=0;
+				cp.currentIteratedNumber = primePlay[cp.previousIteratedNumber]; 
+
+			}
+
+		}
+		printf("\n");
+		
+	}		
+	int displayRowCounter = 0;
+	for(int j = 2; j < CHECKUPTO; ) {
+	  
+	   printf("   %d",j);
+	  
+		displayRowCounter++;
+		if (displayRowCounter==10){
+			printf("\n");
+			displayRowCounter=0;
+		}
+	  
+	  j = primePlay[j];
 	}
 	printf("\n");
 /*
